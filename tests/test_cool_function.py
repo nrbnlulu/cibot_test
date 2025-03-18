1974676434783b4d08f5f011f792fd219ab0b983
nir <nrbnlulu@gmail.com>
2025-03-18 13:55:37 +0200

    fdsafds

--- tests/test_cool_function.py
+++ tests/test_cool_function.py
@@ -1,4 +1,9 @@
-from cool_package import cool_function
+from cool_package import cool_function, not_covered, this_is_not_covered
 
 def test_cool_function():
     assert cool_function() == "Hello from cool_function!"
+
+
+def test_not_covered():
+    not_covered()  # not covered by tests
+    this_is_not_covered()
