d96ce56fc4174a9095c9dc35a37e50cfa830f75b
nir <nrbnlulu@gmail.com>
2025-03-18 12:07:35 +0200

    fdsaf

--- src/cool_package/__init__.py
+++ src/cool_package/__init__.py
@@ -3,9 +3,9 @@ def cool_function():
     return "Hello from cool_function!"
 
 def this_is_not_covered():
-    "not covered by tests"
-    "not covered by tests"
-    return "This is not covered by tests"
+    woo = "not covered by tests"
+    ops = "not covered by tests"
+    return woo + ops
 
 def not_covered():
     return "This is not covered by tests"
